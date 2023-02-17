b2ac34a8971387b03b70ee2310e2b931deb3e9da
Chuchkalova Anastasiya <chuchanastya003@gmail.com>
2023-02-17 12:23:20 +0400

    add part 03 of task (add console app with using nuget package)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿var stack = new Wintellect.PowerCollections.Stack<int>(10);
+
+for (int i = 0; i < stack.Capacity; i++)
+{
+    stack.Push(i);
+}
+
+Console.WriteLine($"Stack Count: {stack.Count}");
+
+while(stack.Count > 0)
+{
+    Console.WriteLine($"Pop item: {stack.Pop()}");
+}
+
+Console.WriteLine($"Stack Count: {stack.Count}");
