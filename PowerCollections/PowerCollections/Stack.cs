d4a1b9ab1dd857c8eeb938b194328af8ec2b0c47
Chuchkalova Anastasiya <chuchanastya003@gmail.com>
2023-03-10 07:23:52 +0400

    add documentation project

--- PowerCollections/PowerCollections/Stack.cs
+++ PowerCollections/PowerCollections/Stack.cs
@@ -5,18 +5,39 @@ using System.Linq;
 
 namespace Wintellect.PowerCollections
 {
+    /// <summary>
+    /// <see cref="Wintellect.PowerCollections.Stack{T}"/> is a collection organized like a stack data structure
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Stack<T> : IEnumerable<T>
     {
         private T[] Array; // stack
+        /// <summary>
+        /// Max count of elements that stack can contain
+        /// </summary>
         public int Capacity { get { return Array.Length; } } // максимальный размер стека
+
         private int count = 0; // текущее число элементов в стеке
+
+        /// <summary>
+        /// Current count of elements in stack
+        /// </summary>
         public int Count { get { return count; } }
 
+        /// <summary>
+        /// Creates new stack with specified capacity
+        /// </summary>
+        /// <param name="count">Stack capacity</param>
         public Stack(int count = 100)
         {
             this.Array = new T[count];
         }
 
+        /// <summary>
+        /// Puts element on the top of stack
+        /// </summary>
+        /// <param name="item">Element to push</param>
+        /// <exception cref="Exception"></exception>
         public void Push(T item)
         {
             if (this.count == this.Capacity)
@@ -27,6 +48,11 @@ namespace Wintellect.PowerCollections
             this.Array[this.count++] = item;
         }
 
+        /// <summary>
+        /// Returns top element of stack without removing it
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public T Top()
         {
             if (this.count == 0)
@@ -37,6 +63,11 @@ namespace Wintellect.PowerCollections
             return this.Array[this.count - 1];
         }
 
+        /// <summary>
+        /// Returns top element of stack and removes it
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public T Pop()
         {
             if (count == 0)
@@ -51,6 +82,11 @@ namespace Wintellect.PowerCollections
 
             return returnValue;
         }
+
+        /// <summary>
+        /// Returns Enumerator that iterates stack from top to bottom
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<T> GetEnumerator()
         {
             if (count == 0)
